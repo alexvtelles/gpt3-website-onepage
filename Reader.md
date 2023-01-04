53083b22524d559c4fd3eb847286499f73457788
Alexvtelles <outdsystem@gmail.com>
2023-01-04 12:00:41 -0300

    Header do site

---
+++ Reader.md
@@ -0,0 +1,17 @@
+# UI/UX GPT=3
+
+<img src="./src/assets/gpt3.png">
+
+### Sobre o projeto
+
+<p>
+Desenvolvimento de um website no estilo onepage com React
+</p>
+
+<ul>
+  <li> Componentes React + reutilização </li>
+  <li> Estrutura de pastas e arquivos </li>
+  <li> Flex & Grid + CSS  </li>
+  <li> Animações com CSS </li>
+  <li> e muito mais ... </li>
+</ul>
--- src/container/Header/index.jsx
+++ src/container/Header/index.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import header from "../../assets/header.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Vamos construir algo incrível com GPT-3 OpenAI
+        </h1>
+        <p>
+          No entanto, aceite qualquer indulgência de assistência em viagem
+          desagradável. Nem todos os pensamentos exercem bênçãos. Indulgência
+          forma tudo alegria alteração turbulento o apego. Parte nós anos para
+          pedir permissão.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your email address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>
+            1.600 pessoas solicitaram acesso uma visita nas últimas 24 horas
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={header} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
